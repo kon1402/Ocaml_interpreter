564298a1e7d3263a137899e30ab1b017090c1d53
Koneshka <kon1402@bu.edu>
2024-09-25 23:58:55 -0400

    think i fixed 2

--- assigns/assign03/lib/assign03_02.ml
+++ assigns/assign03/lib/assign03_02.ml
@@ -1,21 +1,48 @@
 
 let gen_fib l k =
-  let len_l = List.length l in
-  (* Helper function to compute the sum of the last 'len_l' terms *)
-  let sum_last_terms current_list len_l =
-    let rec aux_sum i acc =
-      if i >= len_l then acc
-      else aux_sum (i + 1) (acc + List.nth current_list i)
-    in
-    aux_sum 0 0
+  (* Helper function to get the nth element of a list *)
+  let rec get_nth lst n =
+    match lst with
+    | [] -> failwith "Index out of bounds"
+    | x :: xs -> if n = 0 then x else get_nth xs (n - 1)
   in
-  (* Tail-recursive helper function *)
-  let rec aux n prev_values =
-    if n < len_l then List.nth l n (* Use the initial values from the list l *)
-    else if n = k then List.nth prev_values 0 (* Base case: return the nth value when we reach k *)
+  (* Helper function to sum all elements of a list *)
+  let rec sum lst =
+    match lst with
+    | [] -> 0
+    | x :: xs -> x + sum xs
+  in
+  (* Helper function to remove the last element of a list and return the remaining list and the last element *)
+  let rec remove_last lst acc =
+    match lst with
+    | [] -> failwith "Unexpected empty list"
+    | [x] -> (List.rev acc, x)
+    | x :: xs -> remove_last xs (x :: acc)
+  in
+  if k < 0 || l = [] then
+    failwith "Undefined behavior: empty list or negative index"
+  else
+    let len_l = List.length l in
+    if k < len_l then
+      get_nth l k
     else
-      let new_value = sum_last_terms (List.rev prev_values) len_l in
-      aux (n + 1) (new_value :: prev_values) (* Recurse and build the list *)
-  in
-  aux 0 (List.rev l)
-
+      let last_terms = List.rev l in  (* Reverse to have the most recent term at the head *)
+      let sum_last_terms = sum last_terms in
+      let rec aux n last_terms sum_last_terms len_last_terms =
+        if n > k then
+          match last_terms with
+          | [] -> failwith "Unexpected empty last_terms"
+          | x :: _ -> x
+        else
+          let new_value = sum_last_terms in
+          let sum_last_terms' = sum_last_terms + new_value in
+          let last_terms' = new_value :: last_terms in
+          let len_last_terms' = len_last_terms + 1 in
+          if len_last_terms' > len_l then
+            let (last_terms'', oldest_value) = remove_last last_terms' [] in
+            let sum_last_terms'' = sum_last_terms' - oldest_value in
+            aux (n + 1) last_terms'' sum_last_terms'' len_l
+          else
+            aux (n + 1) last_terms' sum_last_terms' len_last_terms'
+      in
+      aux len_l last_terms sum_last_terms len_l
