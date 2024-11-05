e9c8d31e96338e7a4692d873a5e86d0051964e37
Koneshka <kon1402@bu.edu>
2024-11-05 09:16:16 -0500

    added edge cases

--- assigns/interp01/lib/lib.ml
+++ assigns/interp01/lib/lib.ml
@@ -3,7 +3,7 @@ open Utils
 
 
 (* parse function *)
-let parse s = My_parser.parse s
+let parse s = My_parser.parse s (*this is the parse function that uses the parser directly from the parser folder *)
 
 (*s ubstitution helper function to be used in susbt*)
 let rec occurs_free x e =
@@ -37,74 +37,103 @@ let rec subst v x e =
       If (subst v x e1, subst v x e2, subst v x e3)
   | Let (y, e1, e2) ->
       if x = y then
+        (* x is shadowed, only substitute in e1 *)
         Let (y, subst v x e1, e2)
       else if not (occurs_free x e2) then
         Let (y, subst v x e1, e2)
       else
-        (let fresh = Stdlib320.gensym () in
-         Let (fresh, 
-              subst v x e1,
-              subst v x (subst (VFun (fresh, Var fresh)) y e2)))
+        let fresh = gensym () in
+        Let (fresh, 
+             subst v x e1,
+             subst v x (subst (VFun (fresh, Var fresh)) y e2))
   | Fun (y, e1) ->
       if x = y then
         Fun (y, e1)
       else if not (occurs_free x e1) then
         Fun (y, e1)
       else
-        (let fresh = Stdlib320.gensym () in
-         Fun (fresh, subst v x (subst (VFun (fresh, Var fresh)) y e1)))
+        let fresh = gensym () in
+        Fun (fresh, subst v x (subst (VFun (fresh, Var fresh)) y e1))
   | App (e1, e2) ->
       App (subst v x e1, subst v x e2)
 
-
 (* Evaluation function that uses subst in it for semantics *)
-let rec eval e =
+let rec eval (e : expr) : (value, error) result =
   match e with
+  (* Base cases *)
   | Num n -> Ok (VNum n)
   | True -> Ok (VBool true)
   | False -> Ok (VBool false)
   | Unit -> Ok VUnit
   | Var x -> Error (UnknownVar x)
   | Fun (x, e) -> Ok (VFun (x, e))
+  
+  (* Complex expressions *)
   | If (e1, e2, e3) ->
       (match eval e1 with
-      | Ok (VBool true) -> eval e2
-      | Ok (VBool false) -> eval e3
-      | Ok _ -> Error InvalidIfCond
-      | Error e -> Error e)
-  | Bop (op, e1, e2) ->
-      (match eval e1, eval e2 with
-      | Ok v1, Ok v2 ->
-          (match op, v1, v2 with
-          | Add, VNum n1, VNum n2 -> Ok (VNum (n1 + n2))
-          | Sub, VNum n1, VNum n2 -> Ok (VNum (n1 - n2))
-          | Mul, VNum n1, VNum n2 -> Ok (VNum (n1 * n2))
-          | Div, VNum n1, VNum n2 -> if n2 = 0 then Error DivByZero else Ok (VNum (n1 / n2))
-          | Mod, VNum n1, VNum n2 -> if n2 = 0 then Error DivByZero else Ok (VNum (n1 mod n2))
-          | Lt, VNum n1, VNum n2 -> Ok (VBool (n1 < n2))
-          | Lte, VNum n1, VNum n2 -> Ok (VBool (n1 <= n2))
-          | Gt, VNum n1, VNum n2 -> Ok (VBool (n1 > n2))
-          | Gte, VNum n1, VNum n2 -> Ok (VBool (n1 >= n2))
-          | Eq, VNum n1, VNum n2 -> Ok (VBool (n1 = n2))
-          | Neq, VNum n1, VNum n2 -> Ok (VBool (n1 <> n2))
-          | And, VBool b1, VBool b2 -> Ok (VBool (b1 && b2))
-          | Or, VBool b1, VBool b2 -> Ok (VBool (b1 || b2))
-          | _, _, _ -> Error (InvalidArgs op))
-      | Error e, _ -> Error e
-      | _, Error e -> Error e)
+       | Ok (VBool true) -> eval e2
+       | Ok (VBool false) -> eval e3
+       | Ok _ -> Error InvalidIfCond
+       | Error e -> Error e)
+  
+  | Bop (op, e1, e2)  ->
+      (match op with
+       | And ->  (* Short circuit AND *)
+           (match eval e1 with
+            | Ok (VBool false) -> Ok (VBool false)
+            | Ok (VBool true) -> 
+                (match eval e2 with
+                 | Ok (VBool b) -> Ok (VBool b)
+                 | Ok _ -> Error (InvalidArgs And)
+                 | Error e -> Error e)
+            | Ok _ -> Error (InvalidArgs And)
+            | Error e -> Error e)
+       | Or ->   (* Short circuit OR *)
+           (match eval e1 with
+            | Ok (VBool true) -> Ok (VBool true)
+            | Ok (VBool false) -> 
+                (match eval e2 with
+                 | Ok (VBool b) -> Ok (VBool b)
+                 | Ok _ -> Error (InvalidArgs Or)
+                 | Error e -> Error e)
+            | Ok _ -> Error (InvalidArgs Or)
+            | Error e -> Error e)
+       | _ ->    (* Other binary operations *)
+           match eval e1, eval e2 with
+           | Ok v1, Ok v2 ->
+               (match op, v1, v2 with
+                | Add, VNum n1, VNum n2 -> Ok (VNum (n1 + n2))
+                | Sub, VNum n1, VNum n2 -> Ok (VNum (n1 - n2))
+                | Mul, VNum n1, VNum n2 -> Ok (VNum (n1 * n2))
+                | Div, VNum n1, VNum n2 -> 
+                    if n2 = 0 then Error DivByZero
+                    else Ok (VNum (n1 / n2))
+                | Mod, VNum n1, VNum n2 ->
+                    if n2 = 0 then Error DivByZero
+                    else Ok (VNum (n1 mod n2))
+                | Lt, VNum n1, VNum n2 -> Ok (VBool (n1 < n2))
+                | Lte, VNum n1, VNum n2 -> Ok (VBool (n1 <= n2))
+                | Gt, VNum n1, VNum n2 -> Ok (VBool (n1 > n2))
+                | Gte, VNum n1, VNum n2 -> Ok (VBool (n1 >= n2))
+                | Eq, VNum n1, VNum n2 -> Ok (VBool (n1 = n2))
+                | Neq, VNum n1, VNum n2 -> Ok (VBool (n1 <> n2))
+                | _, _, _ -> Error (InvalidArgs op))
+           | Error e, _ -> Error e
+           | _, Error e -> Error e)
+  
   | Let (x, e1, e2) ->
       (match eval e1 with
-      | Ok v1 -> eval (subst v1 x e2)
-      | Error e -> Error e)
+       | Ok v1 -> eval (subst v1 x e2)
+       | Error e -> Error e)
+  
   | App (e1, e2) ->
       (match eval e1 with
-      | Ok (VFun (x, e)) ->
-          (match eval e2 with
-          | Ok v -> eval (subst v x e)
-          | Error e -> Error e)
-      | Ok _ -> Error InvalidApp
-      | Error e -> Error e)
-
+       | Ok (VFun (x, e)) ->
+           (match eval e2 with
+            | Ok v -> eval (subst v x e)
+            | Error e -> Error e)
+       | Ok _ -> Error InvalidApp
+       | Error e -> Error e)
 (* Interpreter function *)
 let interp s =
   match parse s with
