ee0a9c4dd61667676b5e60d122494e06af7db2f3
Koneshka <kon1402@bu.edu>
2024-11-23 22:35:05 -0500

    removed prints

--- assigns/interp02/lib/lib.ml
+++ assigns/interp02/lib/lib.ml
@@ -1,45 +1,10 @@
+open Stdlib320
 open Utils
 
-(* Exception definitions *)
-exception DivByZero
 exception AssertFail
+exception DivByZero
 
-(* String conversion functions for debugging *)
-let rec string_of_expr e =
-  match e with
-  | Unit -> "Unit"
-  | True -> "True"
-  | False -> "False"
-  | Num n -> "Num " ^ string_of_int n
-  | Var x -> "Var " ^ x
-  | If (e1, e2, e3) ->
-      "If (" ^ string_of_expr e1 ^ ", " ^ string_of_expr e2 ^ ", " ^ string_of_expr e3 ^ ")"
-  | Bop (op, e1, e2) ->
-      "Bop (" ^ string_of_bop op ^ ", " ^ string_of_expr e1 ^ ", " ^ string_of_expr e2 ^ ")"
-  | Fun (x, t, e) ->
-      "Fun (" ^ x ^ ": " ^ string_of_ty t ^ ", " ^ string_of_expr e ^ ")"
-  | App (e1, e2) ->
-      "App (" ^ string_of_expr e1 ^ ", " ^ string_of_expr e2 ^ ")"
-  | Let { is_rec; name; ty; value; body } ->
-      let rec_str = if is_rec then "rec " else "" in
-      "Let (" ^ rec_str ^ name ^ ": " ^ string_of_ty ty ^ " = " ^
-      string_of_expr value ^ " in " ^ string_of_expr body ^ ")"
-  | Assert e ->
-      "Assert (" ^ string_of_expr e ^ ")"
-
-(* Removed 'rec' since the function is not recursive *)
-let string_of_value v =
-  match v with
-  | VUnit -> "VUnit"
-  | VBool b -> "VBool " ^ string_of_bool b
-  | VNum n -> "VNum " ^ string_of_int n
-  | VClos { name; arg; body = _; env = _ } ->
-      let name_str = match name with Some n -> n | None -> "None" in
-      "VClos { name = " ^ name_str ^ "; arg = " ^ arg ^ "; <function> }"
-
-
-      
-(* Parsing *)
+(* Parse function *)
 let parse s = My_parser.parse s
 
 (* Desugaring functions *)
@@ -81,39 +46,39 @@ let rec desugar_expr (e : sfexpr) : expr =
       Bop(op, desugar_expr e1, desugar_expr e2)
   | SAssert e ->
       Assert(desugar_expr e)
-(* Main desugaring function *)
+
 let rec desugar (p : prog) : expr =
   match p with
   | [] -> Unit
   | decl :: rest ->
-    let infer_ty e = 
-      match e with
-      | SBop(op, _, _) -> 
-          (match op with
-           | Add | Sub | Mul | Div | Mod -> IntTy
-           | Lt | Lte | Gt | Gte | Eq | Neq -> BoolTy
-           | And | Or -> BoolTy)
-      | SNum _ -> IntTy
-      | STrue | SFalse -> BoolTy  (* Changed from SBool to STrue|SFalse *)
-      | _ -> IntTy
+      let infer_ty e = 
+        match e with
+        | SBop(op, _, _) -> 
+            (match op with
+             | Add | Sub | Mul | Div | Mod -> IntTy
+             | Lt | Lte | Gt | Gte | Eq | Neq -> BoolTy
+             | And | Or -> BoolTy)
+        | SNum _ -> IntTy
+        | STrue | SFalse -> BoolTy
+        | _ -> IntTy
       in
       let func_ty = 
-        if decl.args = [] then 
-          (match decl.value with
-           | SFun { arg = (_, _); args = []; body } -> 
-               FunTy(IntTy, infer_ty body)  (* For unannotated single-arg functions *)
-           | _ -> decl.ty)
-        else List.fold_right
-          (fun (_, arg_ty) ret_ty -> FunTy(arg_ty, ret_ty))
-          decl.args
-          decl.ty 
+        match decl.value with
+        | SFun { arg = (_, _); args = []; body } when decl.args = [] -> 
+            FunTy(IntTy, infer_ty body)
+        | _ -> 
+            if decl.args = [] then decl.ty
+            else List.fold_right
+              (fun (_, arg_ty) ret_ty -> FunTy(arg_ty, ret_ty))
+              decl.args
+              decl.ty
       in
       let func_value = 
         match decl.args with
         | [] -> 
             (match decl.value with
              | SFun { arg = (x, _); args = []; body } ->
-                 Fun(x, IntTy, desugar_expr body)  (* Use IntTy for numeric functions *)
+                 Fun(x, IntTy, desugar_expr body)
              | _ -> desugar_expr decl.value)
         | _ ->
             List.fold_right
@@ -128,6 +93,7 @@ let rec desugar (p : prog) : expr =
         value = func_value;
         body = desugar rest
       }
+
 (* Type checking *)
 let type_of (e : expr) : (ty, error) result =
   let rec type_of_env (env : ty Stdlib320.env) (e : expr) : (ty, error) result =
@@ -149,8 +115,7 @@ let type_of (e : expr) : (ty, error) result =
          | Ok (FunTy (arg_ty, ret_ty)) ->
              (match type_of_env env e2 with
               | Ok t when t = arg_ty -> Ok ret_ty
-              | Ok t ->
-                  Error (FunArgTyErr (arg_ty, t))
+              | Ok t -> Error (FunArgTyErr (arg_ty, t))
               | Error err -> Error err)
          | Ok t -> Error (FunAppTyErr t)
          | Error err -> Error err)
@@ -178,7 +143,7 @@ let type_of (e : expr) : (ty, error) result =
          | Ok t -> Error (LetTyErr (ty, t))
          | Error err -> Error err)
     | Bop (op, e1, e2) ->
-        let expected_ty, result_ty =
+        let (expected_ty, result_ty) =
           match op with
           | Add | Sub | Mul | Div | Mod -> (IntTy, IntTy)
           | Lt | Lte | Gt | Gte | Eq | Neq -> (IntTy, BoolTy)
@@ -188,15 +153,9 @@ let type_of (e : expr) : (ty, error) result =
          | Ok t1 when t1 = expected_ty ->
              (match type_of_env env e2 with
               | Ok t2 when t2 = expected_ty -> Ok result_ty
-              | Ok t2 ->
-                  let error_msg = "Right operand of " ^ string_of_bop op ^ " has type " ^ string_of_ty t2 ^ ", expected " ^ string_of_ty expected_ty in
-                  print_endline error_msg;
-                  Error (OpTyErrR (op, expected_ty, t2))
+              | Ok t2 -> Error (OpTyErrR (op, expected_ty, t2))
               | Error err -> Error err)
-         | Ok t1 ->
-             let error_msg = "Left operand of " ^ string_of_bop op ^ " has type " ^ string_of_ty t1 ^ ", expected " ^ string_of_ty expected_ty in
-             print_endline error_msg;
-             Error (OpTyErrL (op, expected_ty, t1))
+         | Ok t1 -> Error (OpTyErrL (op, expected_ty, t1))
          | Error err -> Error err)
     | Assert e ->
         (match type_of_env env e with
@@ -207,146 +166,86 @@ let type_of (e : expr) : (ty, error) result =
   type_of_env Stdlib320.Env.empty e
 
 (* Evaluation *)
-let eval expr =
-  let rec eval env e =
+let eval (e : expr) : value =
+  let rec eval_env (env : value Stdlib320.env) (e : expr) : value =
     match e with
     | Unit -> VUnit
     | True -> VBool true
     | False -> VBool false
     | Num n -> VNum n
-    | Var x -> (
-        match Env.find_opt x env with
-        | Some v -> v
-        | None -> failwith ("Unbound variable: " ^ x))
-    | If (e1, e2, e3) -> (
-        let v1 = eval env e1 in
-        match v1 with
-        | VBool true -> eval env e2
-        | VBool false -> eval env e3
-        | _ ->
-            failwith "Condition in if expression did not evaluate to a boolean")
-    | Bop (op, e1, e2) -> (
-        let v1 = eval env e1 in
-        match op with
-        | And -> (
-            match v1 with
-            | VBool false -> VBool false
-            | VBool true -> eval env e2
-            | _ -> failwith "Invalid operand for && operator")
-        | Or -> (
-            match v1 with
-            | VBool true -> VBool true
-            | VBool false -> eval env e2
-            | _ -> failwith "Invalid operand for || operator")
-        | _ ->
-            let v2 = eval env e2 in
-            eval_bop op v1 v2)
-    | Fun (x, _, body) -> VClos { name = None; arg = x; body; env }
-    | App (e1, e2) -> (
-        let v1 = eval env e1 in
-        let v2 = eval env e2 in
-        match v1 with
-        | VClos { name; arg; body; env = closure_env } ->
-            let env' = Env.add arg v2 closure_env in
-            let env' =
-              match name with
-              | Some f_name -> Env.add f_name v1 env'
-              | None -> env'
-            in
-            eval env' body
-        | _ -> failwith "Attempted to apply a non-function value")
-    | Let { is_rec; name; ty = _; value; body } ->
-        if is_rec then (
-          match value with
-          | Fun (arg_name, _, fun_body) ->
-              (* Create recursive closure directly *)
-              let rec_closure = 
-                VClos { 
-                  name = Some name;
-                  arg = arg_name;
-                  body = fun_body;
-                  env = env
-                } in
-              let env' = Env.add name rec_closure env in
-              eval env' body
-          | _ -> failwith "Recursive binding must be a function"
-        ) else (
-          let v_value = eval env value in
-          let env' = Env.add name v_value env in
-          eval env' body
-        )
-    | Assert e -> (
-        let v = eval env e in
-        match v with
-        | VBool true -> VUnit
-        | VBool false -> raise AssertFail
-        | _ -> failwith "Assertion did not evaluate to a boolean")
-  and eval_bop op v1 v2 =
-    match op, v1, v2 with
-    | (Add | Sub | Mul | Div | Mod), VNum n1, VNum n2 -> (
-        match op with
-        | Add -> VNum (n1 + n2)
-        | Sub -> VNum (n1 - n2)
-        | Mul -> VNum (n1 * n2)
-        | Div ->
-            if n2 = 0 then raise DivByZero else VNum (n1 / n2)
-        | Mod ->
-            if n2 = 0 then raise DivByZero else VNum (n1 mod n2)
-        | _ -> failwith "Invalid operator")
-    | (Lt | Lte | Gt | Gte), VNum n1, VNum n2 ->
-        let b =
-          match op with
-          | Lt -> n1 < n2
-          | Lte -> n1 <= n2
-          | Gt -> n1 > n2
-          | Gte -> n1 >= n2
-          | _ -> failwith "Invalid operator"
-        in
-        VBool b
-    | (Eq | Neq), VNum n1, VNum n2 ->
-        VBool (match op with
-              | Eq -> n1 = n2
-              | Neq -> n1 <> n2
-              | _ -> failwith "Invalid operator")
-    | (Eq | Neq), VBool b1, VBool b2 ->
-        VBool (match op with 
-              | Eq -> b1 = b2
-              | Neq -> b1 <> b2
-              | _ -> failwith "Invalid operator")
-    | (And | Or), VBool b1, VBool b2 ->
-        let b =
-          match op with
-          | And -> b1 && b2
-          | Or -> b1 || b2
-          | _ -> failwith "Invalid operator"
-        in
-        VBool b
-    | _ -> failwith "Invalid operands for operator"
+    | Var x ->
+        (match Stdlib320.Env.find_opt x env with
+         | Some v -> v
+         | None -> failwith ("Unbound variable: " ^ x))
+    | Fun (x, _, body) ->
+        VClos { name = None; arg = x; body; env }
+    | App (e1, e2) ->
+        let v1 = eval_env env e1 in
+        let v2 = eval_env env e2 in
+        (match v1 with
+         | VClos { name = None; arg; body; env = clos_env } ->
+             let env' = Stdlib320.Env.add arg v2 clos_env in
+             eval_env env' body
+         | VClos { name = Some f; arg; body; env = clos_env } ->
+             let closure = VClos { name = Some f; arg; body; env = clos_env } in
+             let env' = Stdlib320.Env.add arg v2 (Stdlib320.Env.add f closure clos_env) in
+             eval_env env' body
+         | _ -> failwith "Attempting to call a non-function value")
+    | If (e1, e2, e3) ->
+        (match eval_env env e1 with
+         | VBool true -> eval_env env e2
+         | VBool false -> eval_env env e3
+         | _ -> failwith "If condition must be a boolean")
+    | Let { is_rec = false; name; ty = _; value; body } ->
+        let v = eval_env env value in
+        let env' = Stdlib320.Env.add name v env in
+        eval_env env' body
+    | Let { is_rec = true; name; ty = _; value; body } ->
+        (match value with
+         | Fun (arg, _, body') ->
+             let rec_closure = VClos { name = Some name; arg; body = body'; env } in
+             let env' = Stdlib320.Env.add name rec_closure env in
+             eval_env env' body
+         | _ -> failwith "Let-rec must bind to a function")
+    | Bop (op, e1, e2) ->
+        let v1 = eval_env env e1 in
+        let v2 = eval_env env e2 in
+        (match (op, v1, v2) with
+         | (Add, VNum n1, VNum n2) -> VNum (n1 + n2)
+         | (Sub, VNum n1, VNum n2) -> VNum (n1 - n2)
+         | (Mul, VNum n1, VNum n2) -> VNum (n1 * n2)
+         | (Div, VNum n1, VNum n2) ->
+             if n2 = 0 then raise DivByZero 
+             else VNum (n1 / n2)
+         | (Mod, VNum n1, VNum n2) ->
+             if n2 = 0 then raise DivByZero 
+             else VNum (n1 mod n2)
+         | (Lt, VNum n1, VNum n2) -> VBool (n1 < n2)
+         | (Lte, VNum n1, VNum n2) -> VBool (n1 <= n2)
+         | (Gt, VNum n1, VNum n2) -> VBool (n1 > n2)
+         | (Gte, VNum n1, VNum n2) -> VBool (n1 >= n2)
+         | (Eq, VNum n1, VNum n2) -> VBool (n1 = n2)
+         | (Neq, VNum n1, VNum n2) -> VBool (n1 <> n2)
+         | (And, VBool b1, VBool b2) -> VBool (b1 && b2)
+         | (Or, VBool b1, VBool b2) -> VBool (b1 || b2)
+         | _ -> failwith "Invalid operands for binary operator")
+    | Assert e ->
+        (match eval_env env e with
+         | VBool true -> VUnit
+         | VBool false -> raise AssertFail
+         | _ -> failwith "Assert expression must evaluate to a boolean")
   in
-  eval Env.empty expr
-
+  eval_env Stdlib320.Env.empty e
 
 (* Interpreter *)
 let interp (s : string) : (value, error) result =
   match parse s with
-  | None ->
-      print_endline "Parsing failed";
-      Error ParseErr
+  | None -> Error ParseErr
   | Some prog ->
-      print_endline "Parsing succeeded";
       let expr = desugar prog in
-      print_endline ("Desugared expression: " ^ string_of_expr expr);
       match type_of expr with
-      | Error e ->
-          print_endline ("Type checking failed: " ^ err_msg e);
-          Error e
-      | Ok ty ->
-          print_endline ("Type checking succeeded: " ^ string_of_ty ty);
-          try
-            let v = eval expr in
-            print_endline ("Evaluation succeeded: " ^ string_of_value v);
-            Ok v
+      | Error e -> Error e
+      | Ok _ ->
+          try Ok (eval expr)
           with
-          | Failure msg ->
-              print_endline ("Evaluation failed: " ^ msg);
-              failwith msg
+          | Failure msg -> failwith msg
